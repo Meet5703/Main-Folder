5c318b613c97996885655504abfe93295bd87511
Meet5703 <147489256+Meet5703@users.noreply.github.com>
2023-11-28 23:31:15 +0530

    problem3 completed

---
+++ Main folder/js/assignment2/problem3.js
@@ -0,0 +1,17 @@
+// Program for check a , b , c value is positive negative and zero 
+
+let a = 10 , b = -20 , c = 0
+
+a > 0 ? console.log(`${a} is Positive`):
+a < 0 ? console.log(`${a} is Negative`):
+console.log(`${a} is Zero`);
+
+b > 0 ? console.log(`${b} is Positive`):
+b < 0 ? console.log(`${b} is Negative`):
+console.log(`${b} is Zero`);
+
+c > 0 ? console.log(`${c} is Positive`):
+c < 0 ? console.log(`${c} is Negative`):
+console.log(`${c} is Zero`);
+
+
