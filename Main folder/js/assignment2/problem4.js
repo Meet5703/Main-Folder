8602af15dc8f2d9dd16ec1bd3a96fd986734d41c
Meet5703 <147489256+Meet5703@users.noreply.github.com>
2023-11-28 23:49:09 +0530

    problem4 completed

---
+++ Main folder/js/assignment2/problem4.js
@@ -0,0 +1,10 @@
+// Program for check age for vote eligibility 
+
+let age = 16
+
+if (age >= 18){
+    console.log(`Your Age is ${age} there for You are eligible for vote`);
+}  
+else{
+    console.log(`Your Age is ${age} there for You are not eligible for vote`);
+}
