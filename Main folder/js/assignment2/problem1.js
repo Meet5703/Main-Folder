606a554158b015137b179eafec6371283eb40956
Meet5703 <147489256+Meet5703@users.noreply.github.com>
2023-11-28 16:17:53 +0000

    node

---
+++ Main folder/js/assignment2/problem1.js
@@ -0,0 +1,11 @@
+/* usage of ternory operator
+                    The ternary operator is a conditional operator which evaluates either of two expressions – a true expression and a false expression – based on a conditional expression that you provide*/
+/*Syntax*/
+// condition ? expression1True : expression2False
+
+//even odd program
+let a = 11;
+
+a % 2 === 0 ? console.log(`${a} is an even number`) : console.log(`${a} is an odd number`);
+
+
