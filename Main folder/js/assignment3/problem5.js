7f1dbe760c55ed59f9b70a6a0da8caf322d239fa
Meet5703 <147489256+Meet5703@users.noreply.github.com>
2023-11-30 10:09:20 +0530

    problem5 Solved

---
+++ Main folder/js/assignment3/problem5.js
@@ -0,0 +1,12 @@
+let numAscending = 0;
+while (numAscending < 25) {
+    numAscending++;
+    console.log(numAscending);
+}
+
+
+let numDescending = 25
+while(numDescending > 1){
+    numDescending--
+    console.log(numDescending);
+}
