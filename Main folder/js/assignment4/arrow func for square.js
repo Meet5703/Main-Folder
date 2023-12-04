e6ecb29b63e76e174ff53d70a505c27c20009d75
Meet5703 <147489256+Meet5703@users.noreply.github.com>
2023-12-04 12:15:55 +0530

    q1

---
+++ Main folder/js/assignment4/arrow func for square.js
@@ -0,0 +1,10 @@
+let squareOfnum = (x)=>{
+    let a = x*x
+    console.log(`${a} is square of ${x}`);
+    return{x}
+}
+squareOfnum(5)
+squareOfnum(3)
+squareOfnum(54)
+squareOfnum(8)
+squareOfnum(6)
