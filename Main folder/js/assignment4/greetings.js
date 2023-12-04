47cbeea405016f813d725f51dbf04a76aaea528a
Meet5703 <147489256+Meet5703@users.noreply.github.com>
2023-12-04 12:32:12 +0530

    q2

---
+++ Main folder/js/assignment4/greetings.js
@@ -0,0 +1,7 @@
+function generateGreeting(name){
+    console.log(`hii, ${name} Welcome To our Website`);
+    return(name)
+}
+const person1 = generateGreeting("Milan")
+const person2 = generateGreeting("Kavita")
+const person3 = generateGreeting("Raj")
