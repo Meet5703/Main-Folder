c429489ed12f4dd1753d46c64f3a902e12f09e58
Meet5703 <147489256+Meet5703@users.noreply.github.com>
2023-12-05 10:23:22 +0530

    q5 answered

--- Main folder/js/assignment4/currying.js
+++ Main folder/js/assignment4/currying.js
@@ -1,1 +1,10 @@
-hii jasjotbhai
+function curry (x){//this is main function
+    return function(y){//this is function as argument called as return value
+        return function(z){
+            return x + y + z//this is final return value where all code will executed
+        }
+    }
+}
+console.log(curry(10))//this will return function y
+console.log(curry(10)(28))//this will return function z
+console.log(curry(10)(28)(34))//this will return output
