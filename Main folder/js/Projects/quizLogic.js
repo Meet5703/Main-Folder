a64d35d4c0f226053bdcbfee9ab3be9b3d1d7c09
Meet Khetani <147489256+Meet5703@users.noreply.github.com>
2023-12-07 20:47:05 +0530

    logic maden

--- Main folder/js/Projects/quizLogic.js
+++ Main folder/js/Projects/quizLogic.js
@@ -1,11 +1,31 @@
-function quizAsking(askQuestion, arrayAnsopt, correctAns) {
-  console.log(askQuestion);
-  let a, b, c, d;
-  arrayAnsopt = {
-    a,
-    b,
-    c,
-    d,
-  };
-  console.log(arrayAnsopt);
+function quiz(que, a, b, c, d) {
+  console.log(que);
+  console.log(`A: ${a}`);
+  console.log(`B: ${b}`);
+  console.log(`C: ${c}`);
+  console.log(`D: ${d}`);
+  return [que, a, b, c, d];
 }
+
+function checkAnswer(userAnswer, correctAnswer) {
+  console.log(`Ans - ${userAnswer}`);
+  let score = 0;
+  if (userAnswer === correctAnswer) {
+    score = score + 10;
+    console.log(`Your score is now ${score}, Keep Going Good Work`);
+  } else {
+    console.log("Answer is Wrong, but it's okay, there's no negative marking.");
+    console.log(`Correct Answer is ${correctAnswer}`);
+  }
+}
+
+quiz("What is the value of 2 + 2 ?", 1, 2, 3, 4);
+checkAnswer(2, 4);
+quiz("What is the value of 2 + 2 ?", 1, 2, 3, 4);
+checkAnswer(2, 4);
+quiz("What is the value of 2 + 2 ?", 1, 2, 3, 4);
+checkAnswer(2, 4);
+quiz("What is the value of 2 + 2 ?", 1, 2, 3, 4);
+checkAnswer(2, 4);
+quiz("What is the value of 2 + 2 ?", 1, 2, 3, 4);
+checkAnswer(2, 4);
