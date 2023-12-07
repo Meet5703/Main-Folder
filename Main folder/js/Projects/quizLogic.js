110e95817a41af560817fe3fc256e8acef46e03f
Meet Khetani <147489256+Meet5703@users.noreply.github.com>
2023-12-07 11:12:04 +0530

    1

---
+++ Main folder/js/Projects/quizLogic.js
@@ -0,0 +1,11 @@
+function quizAsking(askQuestion, arrayAnsopt, correctAns) {
+  console.log(askQuestion);
+  let a, b, c, d;
+  arrayAnsopt = {
+    a,
+    b,
+    c,
+    d,
+  };
+  console.log(arrayAnsopt);
+}
