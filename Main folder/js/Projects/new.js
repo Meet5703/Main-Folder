d77b7e8708c2cee9e248ab375a60480ec2be4952
Meet Khetani <147489256+Meet5703@users.noreply.github.com>
2023-12-10 12:27:20 +0530

    10-12-23

--- Main folder/js/Projects/new.js
+++ Main folder/js/Projects/new.js
@@ -1,97 +1,135 @@
-var que = document.querySelector("#question");
-var optA = document.querySelector("#labelOptA");
-var optB = document.querySelector("#labelOptB");
-var optC = document.querySelector("#labelOptC");
-var optD = document.querySelector("#labelOptD");
-var score = document.querySelector("#score");
-var btnPrevious = document.querySelector("#previousQ");
-var btnNext = document.querySelector("#nextQ");
-var correctAnswer;
-var optNotchkd = document.querySelector("#optnotchkd");
-var btnSubmit = document.querySelector("#submitAnswer");
-var queCount = document.querySelector("#quect");
+let que = document.getElementById("question");
+let options = document.getElementsByClassName("opt");
+let score = document.getElementById("score");
+let btnPrevious = document.getElementById("previousQ");
+let btnNext = document.getElementById("nextQ");
+let optNotchkd = document.getElementById("optnotchkd");
+let btnSubmit = document.getElementById("submitAnswer");
+let queCount = document.getElementById("quect");
 
 let objOFques = [
   {
+    question: "What is the capital of India?",
+    options: ["A - Mumbai", "B - Kolkata", "C - New Delhi", "D - Chennai"],
+    correctAnswer: "C - New Delhi",
+  },
+  {
     question: "Which planet is known as the 'Red Planet'?",
-    options: {
-      a: "A - Jupiter",
-      b: "B - Mars",
-      c: "C - Venus",
-      d: "D - Saturn",
-    },
+    options: ["A - Jupiter", "B - Mars", "C - Venus", "D - Saturn"],
     correctAnswer: "B - Mars",
   },
   {
-    question: "what is value of 2 - 1  ??",
-    options: { a: `A - ${1}`, b: `B - ${2}`, c: `C - ${3}`, d: `D - ${4}` },
-    correctAnswer: "1",
+    question: "Which planet is known as the 'Red Planet'?",
+    options: ["A - Jupiter", "B - Mars", "C - Venus", "D - Saturn"],
+    correctAnswer: "B - Mars",
   },
   {
-    question: "what is value of 2 - 1 + 2  ??",
-    options: { a: `A - ${1}`, b: `B - ${2}`, c: `C - ${3}`, d: `D - ${4}` },
-    correctAnswer: 3,
+    question: "Which planet is known as the 'Red Planet'?",
+    options: ["A - Jupiter", "B - Mars", "C - Venus", "D - Saturn"],
+    correctAnswer: "B - Mars",
   },
   {
-    question: "what is value of 2 - 1 + 2  ??",
-    options: { a: `A - ${1}`, b: `B - ${2}`, c: `C - ${3}`, d: `D - ${4}` },
-    correctAnswer: "3",
+    question: "Which planet is known as the 'Red Planet'?",
+    options: ["A - Jupiter", "B - Mars", "C - Venus", "D - Saturn"],
+    correctAnswer: "B - Mars",
   },
   {
-    question: "what is value of 2 - 1 + 2  ??",
-    options: { a: `A - ${1}`, b: `B - ${2}`, c: `C - ${3}`, d: `D - ${4}` },
-    correctAnswer: "3",
+    question: "Which planet is known as the 'Red Planet'?",
+    options: ["A - Jupiter", "B - Mars", "C - Venus", "D - Saturn"],
+    correctAnswer: "B - Mars",
   },
   {
-    question: "what is value of 2 - 1 + 2  ??",
-    options: { a: `A - ${1}`, b: `B - ${2}`, c: `C - ${3}`, d: `D - ${4}` },
-    correctAnswer: "3",
+    question: "Which planet is known as the 'Red Planet'?",
+    options: ["A - Jupiter", "B - Mars", "C - Venus", "D - Saturn"],
+    correctAnswer: "B - Mars",
   },
   {
-    question: "what is value of 2 - 1 + 2  ??",
-    options: { a: `A - ${1}`, b: `B - ${2}`, c: `C - ${3}`, d: `D - ${4}` },
-    correctAnswer: "3",
+    question: "Which planet is known as the 'Red Planet'?",
+    options: ["A - Jupiter", "B - Mars", "C - Venus", "D - Saturn"],
+    correctAnswer: "B - Mars",
   },
   {
-    question: "what is value of 2 - 1 + 2  ??",
-    options: { a: `A - ${1}`, b: `B - ${2}`, c: `C - ${3}`, d: `D - ${4}` },
-    correctAnswer: "3",
+    question: "Which planet is known as the 'Red Planet'?",
+    options: ["A - Jupiter", "B - Mars", "C - Venus", "D - Saturn"],
+    correctAnswer: "B - Mars",
   },
   {
-    question: "what is value of 2 - 1 + 2  ??",
-    options: { a: `A - ${1}`, b: `B - ${2}`, c: `C - ${3}`, d: `D - ${4}` },
-    correctAnswer: "3",
+    question: "Which planet is known as the 'Red Planet'?",
+    options: ["A - Jupiter", "B - Mars", "C - Venus", "D - Saturn"],
+    correctAnswer: "B - Mars",
   },
-  {
-    question: "what is value of 2 - 1 + 2  ??",
-    options: { a: `A - ${1}`, b: `B - ${2}`, c: `C - ${3}`, d: `D - ${4}` },
-    correctAnswer: "3",
-  },
+  // Include other questions here...
 ];
 
 let index = 0;
 let initialscore = 0;
 let queCt = 1;
-
 let totalQuestions = objOFques.length;
 
 function queBank() {
   let queNow = objOFques[index];
   que.innerHTML = queNow.question;
-  optA.innerHTML = queNow.options.a;
-  optB.innerHTML = queNow.options.b;
-  optC.innerHTML = queNow.options.c;
-  optD.innerHTML = queNow.options.d;
-  score.innerHTML = `Score - ${initialscore}`;
-  queCount.innerHTML = `Question - ${queCt} / ${totalQuestions}`;
+
+  for (let i = 0; i < options.length; i++) {
+    options[i].innerHTML = queNow.options[i];
+  }
+
+  score.textContent = `Score - ${initialscore}`;
+  queCount.textContent = `Question - ${queCt} / ${totalQuestions}`;
   return queNow;
 }
+let flagStop = 0;
+// Answer check function
+btnSubmit.addEventListener("click", function checkAns() {
+  let queNow = objOFques[index];
+  let inputName = document.querySelector("input[name=option]:checked");
 
+  if (inputName) {
+    if (flagStop === 0) {
+      let userAnswer = inputName.value;
+      let correctValue = queNow.correctAnswer;
+
+      if (
+        userAnswer === correctValue ||
+        queNow.options[userAnswer - 1] === correctValue
+      ) {
+        initialscore += 10;
+        score.textContent = `Score - ${initialscore}`;
+        flagStop = 1;
+      }
+    }
+  } else {
+    optNotchkd.innerHTML = "<h4>Please select an option</h4>";
+  }
+});
+
+// Previous and Next button logic
 btnNext.addEventListener("click", function () {
-  if (index < objOFques.length) {
+  let queNow = objOFques[index];
+  let inputName = document.querySelector("input[name=option]:checked");
+
+  if (inputName) {
+    if (flagStop === 0) {
+      let userAnswer = inputName.value;
+      let correctValue = queNow.correctAnswer;
+
+      if (
+        userAnswer === correctValue ||
+        queNow.options[userAnswer - 1] === correctValue
+      ) {
+        initialscore += 10;
+        score.textContent = `Score - ${initialscore}`;
+        flagStop = 1;
+      }
+    }
+  } else {
+    optNotchkd.innerHTML = "<h4>Please select an option</h4>";
+  }
+  if (index < totalQuestions - 1) {
     index++;
     queCt++;
     queBank();
+    flagStop = 0;
   }
 });
 
@@ -100,42 +138,6 @@ btnPrevious.addEventListener("click", function () {
     index--;
     queCt--;
     queBank();
-  }
-});
-
-// Answer check function
-stopInfiniteScore = 0;
-
-btnSubmit.addEventListener("click", function checkAns() {
-  let queNow = objOFques[index];
-  let inputName = document.querySelector("input[name=option]:checked");
-
-  if (inputName) {
-    let userAnswer = inputName.value;
-    let correctValue = queNow.correctAnswer.toString().trim().toLowerCase();
-
-    // Check if the user's answer matches the correct answer
-    if (isNaN(userAnswer) || isNaN(correctValue)) {
-      // If either answer is not a number, compare as strings
-      if (userAnswer === correctValue) {
-        if (stopInfiniteScore === 0) {
-          initialscore += 10;
-          score.innerHTML = `Score - ${initialscore}`;
-          stopInfiniteScore = 1;
-        }
-      }
-    } else {
-      // If both are numeric, compare as numbers
-      if (parseFloat(userAnswer) === parseFloat(correctValue)) {
-        if (stopInfiniteScore === 0) {
-          initialscore += 10;
-          score.innerHTML = `Score - ${initialscore}`;
-          stopInfiniteScore = 1;
-        }
-      }
-    }
-  } else {
-    optNotchkd.innerHTML = "Please select an option";
   }
 });
 
